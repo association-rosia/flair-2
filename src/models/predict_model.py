643ffb5c17e62961b0d254312c5c8cac40eab22d
Louis REBERGA <louis.reberga@gmail.com>
2023-09-06 19:46:59 +0200

    polished-morning-36-g3ass16c

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -120,5 +120,5 @@ class FLAIR2Submission:
 
 if __name__ == '__main__':
     sub = FLAIR2Submission()
-    run_name = 'leafy-blaze-19-3vmiyimu'
+    run_name = 'polished-morning-36-g3ass16c'
     sub(run_name=run_name)
