8af032ba969a05f59a3d7f834d5fc998977152f2
Louis REBERGA <louis.reberga@gmail.com>
2023-09-07 13:42:08 +0200

    new class_weights values

--- src/data/class_weights.py
+++ src/data/class_weights.py
@@ -1,6 +1,13 @@
 import pandas as pd
+from scipy.special import softmax
+import numpy as np
 
 df = pd.read_csv('../../data/raw/labels-statistics-12.csv')
 df['Freq.-train (%)'] = df['Freq.-train (%)'].apply(lambda x: 1 / x)
 
-print(list(df['Freq.-train (%)']))
+class_weights = softmax(list(df['Freq.-train (%)']))
+class_weights_bis = np.append(softmax(class_weights[:-1]), [0])
+
+print(class_weights)
+print(class_weights_bis)
+
