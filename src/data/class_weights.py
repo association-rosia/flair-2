d66207218ef60f370a407a089b83e5865e5a85a9
Louis REBERGA <louis.reberga@gmail.com>
2023-09-09 23:12:09 +0200

    test class_weights

--- src/data/class_weights.py
+++ src/data/class_weights.py
@@ -3,11 +3,12 @@ from scipy.special import softmax
 import numpy as np
 
 df = pd.read_csv('../../data/raw/labels-statistics-12.csv')
-df['Freq.-train (%)'] = df['Freq.-train (%)'].apply(lambda x: 1 / x)
+# df['Freq.-train (%)'] = df['Freq.-train (%)'].apply(lambda x: 1 / x)
+df['Freq.-test (%)'] = df['Freq.-test (%)'].apply(lambda x: 1 / x)
 
-class_weights = softmax(list(df['Freq.-train (%)']))
-class_weights_bis = np.append(softmax(class_weights[:-1]), [0])
+class_weights = softmax(list(df['Freq.-test (%)']))
+class_weights_bis = np.append(softmax(list(df['Freq.-test (%)'])[:-1]), [0])
 
-print(class_weights)
-print(class_weights_bis)
+print(list(class_weights))
+print(list(class_weights_bis))
 
