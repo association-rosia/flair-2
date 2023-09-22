522f1f288bad9c5339bc798a66290e65862322a7
Louis REBERGA <louis.reberga@gmail.com>
2023-09-22 19:33:55 +0200

    update assemble

--- src/models/predict_models.py
+++ src/models/predict_models.py
@@ -32,7 +32,7 @@ def create_list_objects(names):
     return models, iterators
 
 
-def predict(models, iterators, path_predictions):
+def predict(models, iterators, path_predictions, save_predictions):
     for batches in tqdm(zip(*iterators)):
         image_ids = None
         outputs = torch.zeros((10, 13, 512, 512)).cuda()
@@ -48,11 +48,13 @@ def predict(models, iterators, path_predictions):
             outputs = torch.add(outputs, output)
 
         outputs = outputs.argmax(dim=1)
-        # for pred_label, img_id in zip(outputs, image_ids):
-        #     img = pred_label.numpy(force=True)
-        #     img = img.astype(dtype=np.uint8)
-        #     img_path = os.path.join(path_predictions, f'PRED_{img_id}')
-        #     tiff.imwrite(img_path, img, dtype=np.uint8, compression='LZW')
+
+        if save_predictions:
+            for pred_label, img_id in zip(outputs, image_ids):
+                img = pred_label.numpy(force=True)
+                img = img.astype(dtype=np.uint8)
+                img_path = os.path.join(path_predictions, f'PRED_{img_id}')
+                tiff.imwrite(img_path, img, dtype=np.uint8, compression='LZW')
 
 
 if __name__ == '__main__':
@@ -63,4 +65,4 @@ if __name__ == '__main__':
     path_predictions = ''
 
     models, iterators = create_list_objects(args.names)
-    predict(models, iterators, path_predictions)
+    predict(models, iterators, path_predictions, save_predictions=False)
