db54e13317b55c55953114d0c13a7c7d45066ea2
Louis REBERGA <louis.reberga@gmail.com>
2023-09-22 19:07:23 +0200

    bugfix range

--- src/models/predict_models.py
+++ src/models/predict_models.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 sys.path.append(os.curdir)
 
 from src.models.lightning import FLAIR2Lightning
@@ -7,35 +8,41 @@ from src.models.lightning import FLAIR2Lightning
 import argparse
 
 from src.constants import get_constants
+
 cst = get_constants()
 
-parser = argparse.ArgumentParser(description='Script for creating submissions with specified models names')
-parser.add_argument('-n', '--names', nargs='+', type=str, help='Name of models to use for submissions')
-args = parser.parse_args()
 
-lightning_models = []
-dataloaders = []
-for name in args.names:
-    lightning_ckpt = os.path.join(cst.path_models, f'{name}.ckpt')
-    lightning_model = FLAIR2Lightning.load_from_checkpoint(lightning_ckpt)
-    lightning_models.append(lightning_model.model.cuda())
-    dataloaders.append(lightning_model.test_dataloader())
+def create_list_objects(names):
+    models = []
+    dataloaders = []
 
+    for name in names:
+        lightning_ckpt = os.path.join(cst.path_models, f'{name}.ckpt')
+        lightning_model = FLAIR2Lightning.load_from_checkpoint(lightning_ckpt)
+        models.append(lightning_model.model.cuda())
+        dataloaders.append(lightning_model.test_dataloader())
 
-iterators = [iter(loader) for loader in dataloaders]
+    iterators = [iter(loader) for loader in dataloaders]
 
-for batches in zip(*iterators):
-    for i, batch in enumerate(batches):
-        image_ids, aerial, sen, _ = batch
-        print(image_ids)
-
-    break
+    return models, iterators
 
 
-# TODO: create test dataloader
+def predict(models, iterators):
+    for batches in zip(*iterators):
+        for i, batch in enumerate(batches):
+            image_ids, aerial, sen, _ = batch
+            aerial = aerial.cuda()
+            sen = sen.cuda()
 
-# TODO: load n models in GPUs
+            print(image_ids)
 
-# TODO: infer without saving to calculate the inference time
+        break
 
-# TODO: infer with saving to create the submission folder
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script for creating submissions with specified models names')
+    parser.add_argument('-n', '--names', nargs='+', type=str, help='Name of models to use for submissions')
+    args = parser.parse_args()
+
+    models, iterators = create_list_objects(args.names)
+    predict(models, iterators)
