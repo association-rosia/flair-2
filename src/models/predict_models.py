eeea9e7a61bfcd5c47b313d5887b5e37d1bfbba7
Louis REBERGA <louis.reberga@gmail.com>
2023-09-27 14:26:23 +0200

    bugfix one_vs_all

--- src/models/predict_models.py
+++ src/models/predict_models.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     path_predictions = os.path.join(cst.path_submissions, run_names)
     os.makedirs(path_predictions, exist_ok=True)
 
-    test_batch_size = 8
+    test_batch_size = 4
     test_num_workers = 10
 
     models, weights, dataloader = create_list_objects(args.names,
