91c598469fb31759ce4972a24bc4804e9c582d96
Louis REBERGA <louis.reberga@gmail.com>
2023-09-27 13:13:38 +0200

    bugfix one_vs_all

--- src/models/predict_models.py
+++ src/models/predict_models.py
@@ -54,6 +54,7 @@ def create_list_objects(names, weights, test_batch_size, test_num_workers):
         prob_cover=10,
         use_augmentation=True,
         use_tta=False,
+        one_vs_all=None,
         is_val=False,
         is_test=True,
     )
